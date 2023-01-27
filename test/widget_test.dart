1d553155a16b1cdcf623a6edd228505c5d6f69c6
sieun <21800405@handong.ac.kr>
2023-01-27 16:49:51 +0900

    rebase error check

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hackathon/bounding.dart';
+
+import 'package:hackathon/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
