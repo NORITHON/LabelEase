075414f5076cf1c63e1e8cbf0cbeb69941ca4f4e
JinseoKim <eunoia@handong.ac.kr>
2023-01-27 15:21:16 +0900

    firebase_data_load

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hackathon/project_service.dart';
+import 'package:provider/provider.dart';
 
+import 'auth_service.dart';
 import 'firebase_options.dart';
+
+final AuthService authService = AuthService();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // main 함수에서 async 사용하기 위함
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ProjectService(),
+      builder: (context, _) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -42,9 +53,68 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    authService.signIn;
     return Scaffold(
       body: Center(
-        child: Column(),
+        child: Consumer<ProjectService>(builder: (context, projectService, _) {
+          return FutureBuilder<QuerySnapshot>(
+              future: projectService.read(),
+              builder: (context, snapshot) {
+                final docs = snapshot.data?.docs ?? [];
+                return ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    final doc = docs[index];
+                    String name = doc.get('name');
+                    String field = doc.get('field');
+                    String annotation = doc.get('annotation');
+                    int count = doc.get('count');
+                    int reward = doc.get('reward');
+                    return ListTile(
+                      title: Text(name),
+                      subtitle: Row(
+                        children: [
+                          Text("$reward"),
+                          Text(annotation),
+                        ],
+                      ),
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              insetPadding:
+                                  const EdgeInsets.fromLTRB(0, 80, 0, 80),
+                              actions: [
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text('시작'),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                              content: Column(
+                                children: [
+                                  Text(name),
+                                  Text("$count"),
+                                  Text(field),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                );
+              });
+        }),
       ),
     );
   }
