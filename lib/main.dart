acba182ac7fc093e8c482b45572ca248ae4b1475
JinseoKim <eunoia@handong.ac.kr>
2023-02-22 01:55:14 +0900

    debugBanner False

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         textTheme: const TextTheme(
